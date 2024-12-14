6a297d2fbd881a2389ce58c81a6b73ba9cdbb4fc
Vishal Parmar <vishalparmar6958@gmail.com>
2024-12-14 17:10:07 +0530

    price calculator project practice

---
+++ price_calculator_project/go.mod
@@ -0,0 +1,3 @@
+module price-calculator-example
+
+go 1.23.3
---
+++ price_calculator_project/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"price-calculator-example/filemanager"
+	"price-calculator-example/prices"
+)
+
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRates {
+		fm := filemanager.New("pricesss.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
